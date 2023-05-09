93637b25f488b749889f0ffaf6471d61073f4855
joaovictorvds <joao-victor275@hotmail.com>
2023-05-08 23:37:00 -0300

    aula 13 e 14 cucumber e runner

---
+++ src/test/java/br/com/chronosacademy/runner/runnerTest.java
@@ -0,0 +1,17 @@
+package br.com.chronosacademy.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/feature/",
+        tags =  "",
+        glue = "src/test/java/br/com/chronosacademy/steps",
+        plugin = {"json:target/reports/cucumberReports.json", "pretty"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+
+public class runnerTest {
+}
